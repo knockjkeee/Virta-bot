3bc1ca35d0933839c7ba1d86cc375e8b95a81be7
knockjkeee <knockjkeee@gmail.com>
2022-06-14 16:24:43 +0300

    restruct baseCore

--- webService/src/main/java/ru/newsystems/webservice/VirtaWebServiceApplication.java
+++ webService/src/main/java/ru/newsystems/webservice/VirtaWebServiceApplication.java
@@ -3,10 +3,9 @@ package ru.newsystems.webservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"ru.newsystems.webservice.*"})
+@SpringBootApplication(scanBasePackages = {"ru.newsystems.webservice.*", "ru.newsystems.basecore.*"})
 public class VirtaWebServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(VirtaWebServiceApplication.class, args);
     }
-
 }
