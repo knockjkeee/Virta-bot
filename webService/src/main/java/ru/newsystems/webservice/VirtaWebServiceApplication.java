f70c220ed69874d900d8d997da88cdf1fdf13053
knockjkeee <knockjkeee@gmail.com>
2022-06-14 15:57:13 +0300

    add webservice

---
+++ webService/src/main/java/ru/newsystems/webservice/VirtaWebServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.newsystems.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"ru.newsystems.webservice.*"})
+public class VirtaWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtaWebServiceApplication.class, args);
+    }
+
+}
