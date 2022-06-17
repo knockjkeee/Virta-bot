00235048382e1af608284dcc6a755ceafe00f0de
knockjkeee <knockjkeee@gmail.com>
2022-06-17 21:47:40 +0300

    refactor parseCommand

--- baseCore/src/main/java/ru/newsystems/basecore/integration/parser/CommandParser.java
+++ baseCore/src/main/java/ru/newsystems/basecore/integration/parser/CommandParser.java
@@ -12,9 +12,6 @@ import java.util.Optional;
 @Component
 public class CommandParser implements Parser {
 
-    private final String PREFIX_FOR_COMMAND = "/";
-//    private final String DELIMITER_COMMAND_BOTNAME = "@";
-
     @Override
     public Optional<ParseDTO> parseCommand(String msg) {
         if (StringUtil.isBlank(msg)) {
@@ -23,9 +20,8 @@ public class CommandParser implements Parser {
         String trimText = StringUtil.trim(msg);
         ImmutablePair<String, String> commandAndText = getDelimitedCommandFromText(trimText);
         if (isCommand(commandAndText.getKey())) {
-            //String commandForParse = cutCommandFromFullText(commandAndText.getKey());
             Optional<Command> command = Command.parseCommand(commandAndText.getKey());
-            return command.map(c -> new ParseDTO(c, commandAndText.getValue()));
+            return command.map(com -> new ParseDTO(com, commandAndText.getValue()));
         }
         return Optional.empty();
     }
@@ -40,13 +36,8 @@ public class CommandParser implements Parser {
         return commandText;
     }
 
-//    private String cutCommandFromFullText(String text) {
-//        return text.contains(DELIMITER_COMMAND_BOTNAME)
-//                ? text.substring(1, text.indexOf(DELIMITER_COMMAND_BOTNAME))
-//                : text.substring(1);
-//    }
-
     private boolean isCommand(String text) {
+        String PREFIX_FOR_COMMAND = "/";
         return text.startsWith(PREFIX_FOR_COMMAND);
     }
 }
