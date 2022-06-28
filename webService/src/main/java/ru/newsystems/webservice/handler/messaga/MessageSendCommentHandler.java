4b9d0aae71ba1b0f65dc004d486ec1895e680f39
knockjkeee <knockjkeee@gmail.com>
2022-06-28 21:19:02 +0300

    add control two messageUpdate for SendOperation [hasText, hasPhoto, hasDocument]

--- webService/src/main/java/ru/newsystems/webservice/handler/messaga/MessageSendCommentHandler.java
+++ webService/src/main/java/ru/newsystems/webservice/handler/messaga/MessageSendCommentHandler.java
@@ -1,8 +1,10 @@
 package ru.newsystems.webservice.handler.messaga;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.GetFile;
 import org.telegram.telegrambots.meta.api.objects.Document;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.newsystems.basecore.integration.VirtaBot;
@@ -30,56 +32,65 @@ public class MessageSendCommentHandler implements MessageHandler {
 
     @Override
     public boolean handleUpdate(Update update) throws TelegramApiException {
-        String text = update.getMessage().getText();
-
-
         if (update.getMessage().getReplyToMessage() != null) {
             List<String> split = Arrays.stream(update.getMessage().getReplyToMessage().getText().split("№"))
                     .map(String::strip)
                     .collect(Collectors.toList());
             if (split.get(0).contains(MessageState.SENDCOMMENT.getName())) {
                 //TODO code
+                // 1 message
+                // 2 photo
+                // 3 document
 
-//                    byte[] decode = Base64.getDecoder().decode(e.getContent().getBytes(StandardCharsets.UTF_8));
-//                    bot.execute(SendDocument.builder()
-//                            .chatId(String.valueOf(update.getCallbackQuery().getMessage().getChatId()))
-//                            .document(new InputFile(new ByteArrayInputStream(decode), e.getFilename()))
-//                            .build());
+                if (update.getMessage().hasText()) {
+                    return true;
+                }
+
+
                 if (update.getMessage().hasPhoto()) {
-                    String fileId = update.getMessage().getPhoto().get(1).getFileId();
-                    GetFile getFile = new GetFile();
-                    getFile.setFileId(fileId);
-                    String filePath = bot.execute(getFile).getFilePath();
-                    File file = bot.downloadFile(filePath);
-                    try {
-                        byte[] encode = Base64.getEncoder().encode(Files.readAllBytes(file.toPath()));
-                        System.out.println("Caption: " + file.getName());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    List<PhotoSize> photos = update.getMessage().getPhoto();
+                    PhotoSize photo = photos.size() == 2 ? photos.get(1) : photos.get(0);
+                    String filePath = prepareBase64(photo.getFileId(), true);
+                    String base64 = getBase64(filePath);
+                    System.out.println("Photo");
+
+                    return true;
 
                 }
 
                 if (update.getMessage().hasDocument()) {
                     Document document = update.getMessage().getDocument();
-                    String fileId = document.getFileId();
-                    GetFile getFile = new GetFile();
-                    getFile.setFileId(fileId);
-                    String filePath = bot.execute(getFile).getFilePath();
-                    File file = bot.downloadFile(filePath);
-                    try {
-                        byte[] encode = Base64.getEncoder().encode(Files.readAllBytes(file.toPath()));
-                        System.out.println("Document: " + file.getName());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    String base64 = prepareBase64(document.getFileId(), false);
+                    System.out.println("Document");
+
+                    return true;
                 }
 
-                return true;
+
+
+                return false;
             }
             return false;
         }
         return false;
+    }
+
+    private String prepareBase64(String fileId, boolean isPhoto) throws TelegramApiException {
+        GetFile getFile = new GetFile();
+        getFile.setFileId(fileId);
+        String filePath = bot.execute(getFile).getFilePath();
+        return isPhoto ? filePath : getBase64(filePath);
+    }
+
+    private @NotNull String getBase64(String filePath) throws TelegramApiException {
+        File file = bot.downloadFile(filePath);
+        try {
+            byte[] encode = Base64.getEncoder().encode(Files.readAllBytes(file.toPath()));
+            return new String(encode);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 
 }
--- webService/src/main/java/ru/newsystems/webservice/handler/update/callback/SendMessageHandler.java
+++ webService/src/main/java/ru/newsystems/webservice/handler/update/callback/SendMessageHandler.java
@@ -2,14 +2,16 @@ package ru.newsystems.webservice.handler.update.callback;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
+import org.telegram.telegrambots.meta.api.methods.ParseMode;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ForceReplyKeyboard;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.newsystems.basecore.integration.VirtaBot;
 import ru.newsystems.basecore.model.dto.callback.SendCommentDTO;
-import ru.newsystems.basecore.model.state.SerializableInlineType;
 import ru.newsystems.basecore.model.state.MessageState;
+import ru.newsystems.basecore.model.state.SerializableInlineType;
 
 @Component
 public class SendMessageHandler extends CallbackUpdateHandler<SendCommentDTO> {
@@ -27,13 +29,21 @@ public class SendMessageHandler extends CallbackUpdateHandler<SendCommentDTO> {
 
   @Override
   protected void handleCallback(Update update, SendCommentDTO dto) throws TelegramApiException {
+//    String text = MessageState.SENDCOMMENT.getName() + " по заявке №" + update.getCallbackQuery().getMessage().getReplyToMessage().getText() + "\n<b>Форма подачи</b>: Заголовок сообщения # Тело сообщения";
     String text = MessageState.SENDCOMMENT.getName() + " по заявке №" + update.getCallbackQuery().getMessage().getReplyToMessage().getText();
     bot.execute(
             SendMessage.builder()
                     .text(text)
                     .replyToMessageId(update.getCallbackQuery().getMessage().getMessageId())
                     .chatId(String.valueOf(update.getCallbackQuery().getMessage().getChatId()))
+                    .parseMode(ParseMode.HTML)
                     .replyMarkup(ForceReplyKeyboard.builder().forceReply(true).build())
                     .build());
+    bot.execute(AnswerCallbackQuery.builder()
+            .cacheTime(10)
+            .text("\t⚠️ Форма подачи: ⚠️\n\n<Заголовок> # <Сообщение>\n\nЗагрузка документов опционально")
+            .showAlert(true)
+            .callbackQueryId(update.getCallbackQuery().getId())
+            .build());
   }
 }
